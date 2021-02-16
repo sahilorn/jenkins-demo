662a55c2262462b9eb3ba4435548b8947863516e
SAHIL BHATIA <sahilorn@gmail.com>
2021-02-16 22:10:21 +0530

    Update createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/sahilorn/gcpDemo.git'
+                    }
+                    branch 'main'
+                }
+            }
+        }
+    }
+}
